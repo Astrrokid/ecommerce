66a2f86ef82f6da93507c7f42c0392da6d8a4636
Astrrokid <kidstarro22@gmail.com>
2023-08-28 17:47:42 +0100

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Navbar} from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        {/* this will be present in every single route while the ones in the routes changes dynamically depending on the page you are in*/}
+      <Navbar/>
+
         {/* to define our router you have to put them inside the routes components */}
         <Routes>
           <Route path='/'/>
